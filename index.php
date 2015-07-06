eb1fac4af8fafa08619222a4c12e8f0b305a6cd7
unknown <Ananay Arora>
2015-07-07 00:33:50 +0530

    yo

--- index.php
+++ index.php
@@ -56,16 +56,39 @@
 	<div class="panel" id="home">
 		<canvas id="canvas" style='position:relative; z-index:-1;'>
 		</canvas>
-		<div class="content" id="about">
-			<h1 class="welcome">Welcome</h1>
-		</div>
+			<!-- <h1 class="welcome">Welcome</h1> -->
+			<div class="card">
+				<div class="card-image">
+					<img src="img/us.png">
+				</div>
+			</div>
+			<div class="card">
+				<div class="card-image">
+					<img src="img/us.png">
+				</div>
+			</div>
+			<div class="card">
+				<div class="card-image">
+					<img src="img/us.png">
+				</div>
+			</div>
 	</div>
 	<div class="panel" id="about">
 		<div class="wrapper">
 			<center>
+			<div class="card">
+				
 				<h1>About</h1>
+				<br />
+				<p>Exun, the computer club at Delhi Public School, R K Puram, stands for “Experts Unlimited”. Our motto is “We not I” , which in essence means that we believe in team spirit. Exun was started in 1992 by the Head of Department, Computer Science, Mr. Mukesh Kumar and a small group of talented students. It is today a 50+ member organisation with its members standing out in various fields like computer programming, computer quizzing, web design & development, digital imaging, 3d design & animation, 2d animation,video and audio editing.
+
+Exun members form an elite team of computer enthusiasts of all ages. They have represented the school in various computer symposia in India and around the world. They have been actively involved in activities like web page designing, software development and attending inter-school competitions. They have won several awards at various symposiums, excelling in nearly all of them and securing top positions.</p>
+			</div>
 			</center>
 		</div>
+	</div>
+	<div class="panel" id="members">
+		
 	</div>
 	<script type="text/javascript" src="js/const.js"></script>
 </body>
